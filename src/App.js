57fcecd6628d027febbf431e09cddadf9716439a
markharley12 <16harleym@gmail.com>
2023-07-16 23:34:12 +0100

    Initial commit - 30days React solution 04 - level2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import cssLogo from './images/css_logo.png'
+import htmlLogo from './images/html_logo.png'
+import jsLogo from './images/js_logo.png'
+import reactLogo from './images/react_logo.png'
+
+const copyRight = '2020'
+const Footer = () => (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>Copyright &copy;{copyRight}</p>
+    </div>
+  </footer>
+)
+
+// User Card Component
+const Image1 = () => (
+  <div className='Image1'>
+    <img src={cssLogo} alt='css Logo' />
+    <img src={htmlLogo} alt='html Logo' />
+    <img src={jsLogo} alt='js Logo' />
+    <img src={reactLogo} alt='react Logo' />
+    <h2>Caption 1</h2>
+  </div>
+)
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div>
+          <Image1 />
+        </div>
+        <div>
+          <Footer />
+        </div>
       </header>
     </div>
   );
