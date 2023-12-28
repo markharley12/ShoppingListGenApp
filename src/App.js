a4a4687e204ed520d9e4d690e4e01f684d1bc7e8
markharley12 <16harleym@gmail.com>
2023-12-28 00:20:53 +0000

    LLM api can now be used to automatically run the list generation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import fetchGoogleImage from './fetchGoogleImage';
+import llamaStreamQnA from './createShoppingList.js';
 import { apiKey, cx } from './Private.js';
 import { selectNDinners, formatListAsSentence, gptShoppingListPrompt, gptShoppingListPrompt2 } from './fetchMeal.js';
 import './App.css';
@@ -7,7 +8,21 @@ import './App.css';
 const App = () => {
     const [searchWord, setSearchWord] = useState('');  // Now it's a single word, for the number of meals.
     const [mealNames, setMealNames] = useState([]);
+    const [shoppingListPrompt, setShoppingListPrompt] = useState([]);
     const [imageUrls, setImageUrls] = useState([]);
+    const [result2, setresult2] = useState([]);
+
+    // New State for Bot messages
+    const [botMessages, setBotMessages] = useState("");
+
+    // Function to handle the streaming of messages
+    const handleLlamaStreamQnA = async (prompt) => {
+        const prev = "";
+        for await (const message of llamaStreamQnA(prompt)) {
+            console.log(message);
+            setBotMessages(prev => prev + message); // Append new message
+        }
+    };
 
     const handleSearch = async () => {
         const numDinners = parseInt(searchWord, 10);
@@ -16,13 +31,18 @@ const App = () => {
             try {
                 const dinners = await selectNDinners(numDinners, './lists/Dinners.md');
                 setMealNames(dinners);
-                console.log(dinners);
+                const shoppingListPrompt1 = gptShoppingListPrompt(formatListAsSentence(dinners));
+                setShoppingListPrompt(shoppingListPrompt1);
+                console.log(shoppingListPrompt1);
 
                 const urls = await Promise.all(dinners.map(async mealName => {
                     return await fetchGoogleImage(mealName, apiKey, cx);
                 }));
-
                 setImageUrls(urls);
+
+                // Call the streaming function with the prompt
+                handleLlamaStreamQnA(shoppingListPrompt1);
+
             } catch (error) {
                 console.error('Error:', error);
             }
@@ -60,8 +80,10 @@ const App = () => {
             
             {/* New addition */}
             <div className="prompt-container">
-                <textarea readOnly value={gptShoppingListPrompt(formatListAsSentence(mealNames))}></textarea>
+                <textarea readOnly value={shoppingListPrompt}></textarea>
+                <textarea readOnly value={botMessages}></textarea>
                 <textarea readOnly value={gptShoppingListPrompt2()}></textarea>
+                <textarea readOnly value={"Result2"}></textarea>
             </div>
         </div>
     );
---
+++ src/createShoppingList.js
@@ -0,0 +1,142 @@
+import axios from 'axios';
+
+const DEBUG = false;
+const llmApiUrl = "http://localhost:8001/completion"; // Replace with actual LLM API URL from your config
+let modelType = "zephyr";  // or "CodeLlama"
+
+const parseStreamStringToJson = (streamString) => {
+    try {
+        const removePrefix = streamString.substring(6);
+        return JSON.parse(removePrefix);
+    } catch (e) {
+        console.error(`Error parsing JSON from line: ${e}`);
+    }
+};
+
+const wrapPrompt = (basePrompt) => {
+    switch(modelType) {
+        case "zephyr":
+            return ` You are a helpful assistant. </s>  ${basePrompt} </s> `;
+        case "CodeLlama":
+            return `
+[INST] Write code to solve the following coding problem that obeys the constraints and passes the example test cases. Please wrap your code answer using \`\`\`:
+${basePrompt}
+[/INST]
+`;
+        default:
+            return basePrompt;
+    }
+};
+
+const getInputJson = (input) => {
+    // Replace this condition with your actual condition or configuration
+    if (llmApiUrl === "http://localhost:8001/v1/chat/completions") {
+        return {
+            messages: [
+                {
+                    content: "You are a helpful assistant.",
+                    role: "system"
+                },
+                {
+                    content: input,
+                    role: "user"
+                }
+            ]
+        };
+    } else {
+        return {
+            prompt: input,
+            max_tokens: 512,
+            stream: true
+        };
+    }
+};
+
+const llamaRequest = async (payload) => {
+    if (DEBUG) {
+        console.log(`${llmApiUrl} : HTTP request POST : ${JSON.stringify(payload)}`);
+    }
+    try {
+        const response = await axios.post(llmApiUrl, payload);
+        return response.data;
+    } catch (error) {
+        console.error("Error in llamaRequest:", error);
+    }
+};
+
+const llamaStreamRequest = async function* (payload) {
+    if (DEBUG) {
+        console.log(`${llmApiUrl} : HTTP request POST : ${JSON.stringify(payload)}`);
+    }
+    try {
+        const response = await fetch(llmApiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // Add any other necessary headers here
+            },
+            body: JSON.stringify(payload)
+        });
+
+        // Ensure the response is ok
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        // Use the reader from the response's body to read the stream
+        const reader = response.body.getReader();
+        let decoder = new TextDecoder();
+
+        while (true) {
+            const { value, done } = await reader.read();
+            if (done) break;
+            let chunk = decoder.decode(value, {stream: true}); // Decode chunk as text
+            // Process the chunk using your parseStreamStringToJson function
+            let jsonContent = parseStreamStringToJson(chunk).content;
+            if (jsonContent) {
+                yield jsonContent; // Yield the JSON content
+            }
+        }
+    } catch (e) {
+        console.error(`An error occurred in llamaStreamRequest: ${e}`);
+    }
+};
+
+
+const llamaQnA = async (question) => {
+    const wrappedQuestion = wrapPrompt(question);
+    const inputJson = getInputJson(wrappedQuestion);
+    try {
+        const response = await llamaRequest(inputJson);
+        console.log(response);
+        return response
+    } catch (error) {
+        console.error("Error in llamaQnA:", error);
+    }
+};
+
+// Modify llamaStreamQnA to be an async generator
+export default async function* llamaStreamQnA(question) {
+    for await (const messageStream of llamaStreamRequest(getInputJson(wrapPrompt(question)))) {
+        // process.stdout.write(messageStream);
+        // console.log(messageStream);
+        yield messageStream;  // Use yield to provide values to the for-await-of loop
+    }
+}
+
+// Main function to demonstrate usage
+const main = async () => {
+    const inputStr="print hello world in python"
+    // const inputStr="I would like to cook the following meals: Crispy baked tofu with sesame noodles, and Vegetable lasagna with garlic bread. What are the ingredients I would need for each of these? Be minimalist and exclude quantities. Group together similar items in the shopping list."
+    console.log(`[User] ${inputStr}`);
+    process.stdout.write("[Bot] ")
+
+    for await (const message of llamaStreamQnA(inputStr)) {
+            process.stdout.write(JSON.stringify(message));
+        }
+
+    console.log("\n\nDone...");
+};
+
+// // Uncomment to run the main function
+// main();
